0f501bf9a4550479b0bf1a350a1cd6306135807e
wessim <hmaied..wessim@gmail.com>
2019-02-28 19:01:22 +0100

    Prepare the game board with 9 stylesh boxes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import Header from './Header.js' 
+import Header from './Header.js'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      boardBoxes: Array(9).fill(null),
+    }
+  }
+
+  renderBoxes() {
+  return this.state.boardBoxes.map(
+    (box, index) =>
+      <div className="box" key={index}>
+        <div className="x-or-o">{box}</div>
+      </div>
+  )
+}
+
   render() {
     return (
       <div className="container">
         <Header />
         <div className="game-board">
+          {this.renderBoxes()}
         </div>
       </div>
     );
