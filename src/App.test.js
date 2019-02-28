2cbd8b0782a5ed45b7785e945bfc860801f176bc
wessim <hmaied..wessim@gmail.com>
2019-02-28 02:57:04 +0100

    import enzyme for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
 import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() });
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className='App-title'>Welcome to React</h1>;
+  });
 });
